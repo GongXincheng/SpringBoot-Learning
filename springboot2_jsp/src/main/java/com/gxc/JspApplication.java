111993a1a5099727fe7ff855bfb3c733ec56f50a
宫新程 <gongxincheng@jsh.com>
2018-09-09 11:12:58 +0800

    deploy with war

--- springboot2_jsp/src/main/java/com/gxc/JspApplication.java
+++ springboot2_jsp/src/main/java/com/gxc/JspApplication.java
@@ -2,9 +2,21 @@ package com.gxc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * SpringBoot打war包部署的时，
+ *  启动类需要继承 SpringBootServletInitializer
+ *  并重写 configure 方法.
+ */
 @SpringBootApplication
-public class JspApplication {
+public class JspApplication extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(JspApplication.class);
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(JspApplication.class, args);
