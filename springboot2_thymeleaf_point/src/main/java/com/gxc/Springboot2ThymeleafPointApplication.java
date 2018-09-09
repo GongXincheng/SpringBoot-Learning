7f76667b5c1625c98354845948d56117d7cdd090
宫新程 <gongxincheng@jsh.com>
2018-09-09 14:35:22 +0800

    thymleaf

---
+++ springboot2_thymeleaf_point/src/main/java/com/gxc/Springboot2ThymeleafPointApplication.java
@@ -0,0 +1,12 @@
+package com.gxc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2ThymeleafPointApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Springboot2ThymeleafPointApplication.class, args);
+  }
+}
